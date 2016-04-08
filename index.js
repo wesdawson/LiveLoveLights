4451bd55ebd816c437a093116878986dde8390aa
wesdawson <wesley.s.dawson@gmail.com>
2016-04-07 20:44:05 -0700

    attempt to serve static page

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+//serve static page
+app.configure(function(){
+  app.use('/media', express.static(__dirname + '/media'));
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.listen(3000, function () {
